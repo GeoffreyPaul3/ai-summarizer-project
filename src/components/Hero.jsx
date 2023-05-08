c024253a43781ad9451405f69f51a0759893b740
GeoffreyPaul3 <110961738+Gtigercode@users.noreply.github.com>
2023-05-08 13:27:26 +0200

    initialized git repository

---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header
+      className="w-full flex justify-center
+     items-center flex-col"
+    >
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/GeoffreyPaul3", "_blank")
+          }
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Summarify is an open-source article summarizer that converts large
+        articles into clear and succinct summaries.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
